709c874e52b30396d48a47cc1e666f3f3eacaa87
Dung Huynh <dung.huynh@gourmetplus.com>
2020-09-04 10:35:55 +0700

    update config

--- src/configs/index.js
+++ src/configs/index.js
@@ -3,6 +3,6 @@ const devMode = process.env.NODE_ENV === 'development';
 export default {
   DEV: devMode,
   SERVER_URL: devMode
-    ? 'http://localhost:5000/api/'
-    : '/api/',
+    ? 'http://localhost:5000/api'
+    : 'https://secure-reef-64350.herokuapp.com/api',
 };
