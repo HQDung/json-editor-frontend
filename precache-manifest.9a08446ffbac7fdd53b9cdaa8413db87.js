937804871c35bf59274d74aabdef48edf5a5c971
Dung Huynh <dung.huynh@gourmetplus.com>
2020-09-04 10:40:55 +0700

    Updates

---
+++ precache-manifest.9a08446ffbac7fdd53b9cdaa8413db87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d21bc5d7f92fbe42377484706b43a3c",
+    "url": "/json-editor-frontend/index.html"
+  },
+  {
+    "revision": "4eb2bceed49b291a6c04",
+    "url": "/json-editor-frontend/static/css/main.552615f4.chunk.css"
+  },
+  {
+    "revision": "40851ae7582051ecd060",
+    "url": "/json-editor-frontend/static/js/2.cac3588d.chunk.js"
+  },
+  {
+    "revision": "4eb2bceed49b291a6c04",
+    "url": "/json-editor-frontend/static/js/main.cf17345c.chunk.js"
+  },
+  {
+    "revision": "a5422a10d9218aad699f",
+    "url": "/json-editor-frontend/static/js/runtime-main.dad29cf9.js"
+  }
+]);
